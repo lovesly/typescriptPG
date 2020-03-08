bd1a398b0024c2b60819ffd674a5c90733a04b66
zzh <zzhlovesly@outlook.com>
2020-03-09 01:09:39 +0800

    resume typescript learning

---
+++ av76565963/examples/greeter.ts
@@ -0,0 +1,26 @@
+class User {
+    firstName: string;
+    lastName: string;
+    fullName: string;
+
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = `${firstName} ${lastName}`
+    }
+}
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return `Hello ${person.firstName} ${person.lastName}`;
+}
+
+// 可以多，不可以少？
+let user: Person = new User('Z', 'S');
+
+console.log(greeter(user));
+// stopped at p4
