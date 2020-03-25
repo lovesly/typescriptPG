270167aee12a9b261274c27f5d7d7dd2e781fcd9
zzh <zzhlovesly@outlook.com>
2020-03-25 22:44:11 +0800

    commit after re-init inner git repo

--- zz-axios/src/zz-axios.ts
+++ zz-axios/src/zz-axios.ts
@@ -4,3 +4,12 @@
 export default class DummyClass {
 
 }
+
+interface Test {
+  name: string;
+}
+
+// Git 仓库嵌套的问题，如果内部，和外部的，指向两个不同的 remote origin，会怎么样？
+const fn = ():void => {
+  console.log('Shit')
+}
